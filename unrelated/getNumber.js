066115e3b634a967b4e35bfa6eeafe439adfe069
ezl-13 <ericzlou@gmail.com>
2019-08-18 15:05:22 -0700

    test

--- unrelated/getNumber.js
+++ unrelated/getNumber.js
@@ -1,4 +1,4 @@
 function getNumber() {
-    var randomNumber = Math.floor(Math.random() * 900 + 100); // Generates random number
-    document.getElementById('myNumber').innerHTML = randomNumber; // Sets content of <div> to number
+    var randomNumber = Math.floor(Math.random() * 900 + 100);
+    document.getElementById('myNumber').innerHTML = randomNumber;
 }
